5a0a3763241224acde0b40317fdede04703fa569
Kleber <kleber@aswti.com.br>
2022-04-27 17:37:54 -0300

    Structs ToDo

--- models/toDoCab.go
+++ models/toDoCab.go
@@ -1,1 +1,6 @@
 package models
+
+type ToDoCab struct {
+	Descricao string `db:"descricao" json:"descricao"`
+	DataHora  string `db:"data_hora" json:"data_hora"`
+}
--- models/toDoIt.go
+++ models/toDoIt.go
@@ -1,1 +1,6 @@
 package models
+
+type ToDoIt struct {
+	Descricao  string `db:"descricao" json:"descricao"`
+	Prioridade string `db:"prioridade" json:"prioridade"`
+}
