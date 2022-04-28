38aee41b73edec3f905ef9ee0a0481b91490ec46
Kleber <kleber@aswti.com.br>
2022-04-28 13:14:06 -0300

    Descomentado função main

--- bin/main.go
+++ bin/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"log"
+	"treinamento-api/database"
+	"treinamento-api/routes"
+
 	_ "github.com/lib/pq"
 )
 
-/*
 func main() {
 	log.Println("Conectando no banco de dados...")
 	database.ConnectDataBase()
 	routes.GetRoutes()
 
 }
-*/
 
 /*
 func main() {
